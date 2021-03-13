9791e538b0b5b16115996a18ba876dc709c67327
Pedro Ortiz <pedrortiz89@gmail.com>
2021-03-12 22:14:10 -0600

    Adding the review section for one item

---
+++ lib/review_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'review.dart';
+
+class ReviewList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final review_list = Container();
+    return review_list;
+  }
+
+}
