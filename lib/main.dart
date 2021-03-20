aee67d600dc8fd91b87162899d98561d8c91aaa6
Pedro Ortiz <pedrortiz89@gmail.com>
2021-03-20 13:20:56 -0600

    Test for jenkins pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,18 @@ import 'dart:core';
 import 'my_trips.dart';
 
 void main() {
-  runApp(MyApp());// para correr la app, punto de inicio de una app en flutter
+  runApp(MyApp()); // para correr la app, punto de inicio de una app en flutter
 }
 
+// testing for jenkins
 // MyApp es una clase que contiene todos los widgets
 // StatelessWidget es la clase padre de los widgets
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { // build es un metodo de StatelessWidget
-    return MaterialApp( // Punto inicial donde toma todos los elementos de la biblioteca llamada
+  Widget build(BuildContext context) {
+    // build es un metodo de StatelessWidget
+    return MaterialApp(
+      // Punto inicial donde toma todos los elementos de la biblioteca llamada
       title: 'My Trips', // titulo para el task manager
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,7 +34,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyTripsCupertino(),
-       //MyHomePage(title: 'Home Page'), // en home es la estructura de la app, la composicion
+      //MyHomePage(title: 'Home Page'), // en home es la estructura de la app, la composicion
       // MyHomePage es un clase heredada de StatelessWidget
       // El Scaffold se incluye en el home, y aqui le indico hacia que widget se debe dirigir cada componenete de la app
     );
@@ -41,4 +44,3 @@ class MyApp extends StatelessWidget {
 // Un widget stateful son los que interactuan y tienen un estado, como un boton
 // Un widget stateless no tienen estado, como texto, icono, y no interactua con el usuario
 // los layouts son las columnas, renglones, views
-
